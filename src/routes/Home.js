dfbec5313383f851598938ffb29fbad70e6a710f
dnlmrtn <danielmartin102001@gmail.com>
2023-08-09 11:32:34 -0400

    Fixed root color

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -24,48 +24,55 @@ const Home = () => {
                         Daniel Martin
                     </div>
                     <div className='text-left'><br /></div>
+
+                    <div className="content-container">
+                        <div className='text-left'>Age</div>
+                        <div className='text-right'>22</div>
+                    </div>
+                    <div className="content-container">
+                        <div className='text-left'>Degree</div>
+                        <div className='text-right'>Math &amp; Eng</div>
+                    </div>
+                    <div className="content-container">
+                        <div className='text-left'>Working on</div>
+                        <div className='text-right'>RecessD</div>
+                    </div>
+                    <div className="content-container">
+                        <div className='text-left'>Fav. Food</div>
+                        <div className='text-right'>Tacos</div>
+                    </div>
+                    <div className="content-container">
+                        <div className='text-left'>Fav. Quote</div>
+
+                        <div className='text-right'></div>
+                    </div>
+
+                    <div className='text-left'>
+                        "What I cannot create I do not understand."
+                    </div>
+                    <div className='italic-text'>
+                        Richard Feynman
+                        <br />
+                        <br />
+
+                    </div>
                     <div className="info-section">
-                        <div className="content-container">
-                            <div className='text-left'>Age</div>
-                            <div className='text-right'>22</div>
-                        </div>
-                        <div className="content-container">
-                            <div className='text-left'>Degree</div>
-                            <div className='text-right'>Math &amp; Eng</div>
-                        </div>
-                        <div className="content-container">
-                            <div className='text-left'>Working on</div>
-                            <div className='text-right'>RecessD</div>
-                        </div>
-                        <div className="content-container">
-                            <div className='text-left'>Fav. Food</div>
-                            <div className='text-right'>Tacos</div>
-                        </div>
-                        <div className="content-container">
-                            <div className='text-left'>Fav. Quote</div>
+                        <div className="text-left">
+                            Finished BASc. at Queen’s in
+                            Math &amp; Engineering, Applied Mechanics option.
+                            Currently building ML-based control systems at RecessD, a startup developing wireless industrial automation.<br />
 
-                            <div className='text-right'></div>
-                        </div>
-                    </div>
-                    <div className='info-section'>
-                        <div className='text-left'>
-                            "What I cannot create I do not understand."
-                        </div>
-                        <div className='italic-text'>
-                            Richard Feynman
                         </div>
 
                     </div>
-                    <div className="text-left">
-                        <br />
-                        Finished BASc. at Queen’s in
-                        Math &amp; Engineering, Applied Mechanics option.
-                        Currently building ML-based control systems at RecessD, a startup developing wireless industrial automation.<br />
-                        <br />
-                        For work history, click About Me.<br />
-                        For passion projects, hit Projects.<br />
-                        For my interests, go to Personal.<br />
-                        Feel free to reach out.
+                    <br />
+                    <div className="info-section">
+                        <div className="text-left">
+                            For work history, click About Me.<br />
+                            For passion projects, hit Projects.<br />
+                            For my interests, go to Personal.<br />
+                            Feel free to reach out.
+                        </div>
                     </div>
                 </div>
             </div>
