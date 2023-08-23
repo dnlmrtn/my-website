cfc85bb1cb8353ed1dc08737f6c992aad089ce20
dnlmrtn <danielmartin102001@gmail.com>
2023-08-22 23:40:18 -0400

    Added personal page content

--- src/routes/Personal.js
+++ src/routes/Personal.js
@@ -1,5 +1,67 @@
 const Personal = () => {
-    return <body>Personal page content coming soon</body>;
+    return <div className="about-container">
+        <div className="about-item">
+            <div className='title-container'>
+                <div className='about-title'>Books</div>
+                <div className='superscript'>
+                    <a href="https://www.goodreads.com/user/show/28072569-daniel-martin" target="_blank" rel="noopener noreferrer">Goodreads</a>
+                </div>
+
+            </div>
+            <div className='about-description' style={{ width: '98%' }}>
+                <div className="content-container" >
+                    <div className='text-left' style={{ fontSize: '19px' }}>Why Greatness Cannot be Planned</div>
+                    <div className='text-right' style={{ fontSize: '19px' }}>Kenneth Stanley</div>
+                </div>
+                <div className="content-container" >
+                    <div className='text-left' style={{ fontSize: '19px' }}>The Moral Landscape</div>
+                    <div className='text-right' style={{ fontSize: '19px' }}>Sam Harris</div>
+                </div>
+                <div className="content-container" >
+                    <div className='text-left' style={{ fontSize: '19px' }}>Win Friends and Influence People</div>
+                    <div className='text-right' style={{ fontSize: '19px' }}>Dale Carnegie</div>
+                </div>
+                <div className="content-container" >
+                    <div className='text-left' style={{ fontSize: '19px' }}>Flow</div>
+                    <div className='text-right' style={{ fontSize: '19px' }}>Mihaly Csikszentmihalyi</div>
+                </div>
+            </div>
+        </div >
+        <div className='vertical-container'>
+            <div className='vertical'>
+            </div>
+        </div>
+        <div className="about-item">
+            <div className='title-container'>
+                <div className='about-title'>Music</div>
+                <div className='superscript'>
+                    <a href="https://open.spotify.com/user/dnlmrtn?si=f5cfcaef941e4796" target="_blank" rel="noopener noreferrer">Spotify</a>
+                </div>
+
+            </div>
+            <div className='about-description' style={{ width: '98%' }}>
+                <div className="content-container" >
+                    <div className='text-left' style={{ fontSize: '19px' }}>can we</div>
+                    <div className='text-right' style={{ fontSize: '19px' }}>
+                        piri & tommy</div>
+                </div>
+                <div className="content-container" >
+                    <div className='text-left' style={{ fontSize: '19px' }}>
+                        I Wish</div>
+                    <div className='text-right' style={{ fontSize: '19px' }}>Stevie Wonder</div>
+                </div>
+                <div className="content-container" >
+                    <div className='text-left' style={{ fontSize: '19px' }}>Conveniency</div>
+                    <div className='text-right' style={{ fontSize: '19px' }}>Nia Archives</div>
+                </div>
+                <div className="content-container" >
+                    <div className='text-left' style={{ fontSize: '19px' }}>Only A Fool Would Say That</div>
+                    <div className='text-right' style={{ fontSize: '19px' }}>
+                        Steely Dan</div>
+                </div>
+            </div>
+        </div >
+    </div >;
 };
 
 export default Personal;
